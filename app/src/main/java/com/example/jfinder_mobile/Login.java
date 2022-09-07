1bfc40456b63a9e24ad46f475bcbdbfa7dbb9742
Maaacs <msb@icomp.ufam.edu.br>
2022-09-07 11:12:33 -0400

    Finalizações UI Design

--- app/src/main/java/com/example/jfinder_mobile/Login.java
+++ app/src/main/java/com/example/jfinder_mobile/Login.java
@@ -4,8 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ActivityOptions;
 import android.content.Intent;
-import android.opengl.Visibility;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -37,7 +37,7 @@ public class Login extends AppCompatActivity {
         });
     }
 
-    public boolean verificarLogin() {
+    public boolean autenticarLogin() {
         EditText nome = (EditText) findViewById(R.id.edit_usuario);
         EditText senha = (EditText) findViewById(R.id.edit_senha);
         String user = nome.getText().toString();
@@ -53,7 +53,15 @@ public class Login extends AppCompatActivity {
     }
 
     public void exibirBar(){
-        bar.setVisibility(true ? View.VISIBLE : View.GONE);
+        /*bar.setVisibility(true ? View.VISIBLE : View.GONE); outra forma de exirbir a barra*/
+        bar.setVisibility(View.VISIBLE);
+    }
+
+    private void proxTela(){
+        Intent nextActivity = new Intent(getApplicationContext(), NavigationMenu.class);
+        /*startActivity(nextActivity, ActivityOptions.makeSceneTransitionAnimation(this).toBundle());*/ // animação
+        startActivity(nextActivity);
+        finish(); //finaliza a view activity ao trocar para a proxima
     }
 
 
@@ -68,20 +76,22 @@ public class Login extends AppCompatActivity {
         if(user.matches("") && password.matches("")){
             Toast.makeText(this, "Insira os dados!", Toast.LENGTH_SHORT).show();
         }else{
-            if (verificarLogin()){
+            if (autenticarLogin()){
 
                 exibirBar();
-                Intent nextActivity = new Intent(getApplicationContext(), NavigationMenu.class);
-                startActivity(nextActivity, ActivityOptions.makeSceneTransitionAnimation(this).toBundle());
+
+                new Handler().postDelayed(new Runnable() { //método para adicionar delay na transição de tela
+                    @Override
+                    public void run() {
+                        proxTela();
+                    }
+                }, 2600);
 
             }else{
                 System.out.println("poxaa");
                 Toast.makeText(this, "Usuário ou senha incorretos!", Toast.LENGTH_SHORT).show();
-
             }
         }
-
-
     }
 
     public void sairBtn(View view) {
