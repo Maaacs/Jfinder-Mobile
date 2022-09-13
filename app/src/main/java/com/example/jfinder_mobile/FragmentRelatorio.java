a451fbdd91ce589c2cfcb6eed94a512f03daca1b
Maaacs <msb@icomp.ufam.edu.br>
2022-09-13 10:59:02 -0400

    Final

--- app/src/main/java/com/example/jfinder_mobile/FragmentRelatorio.java
+++ app/src/main/java/com/example/jfinder_mobile/FragmentRelatorio.java
@@ -11,19 +11,13 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link FragmentRelatorio#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class FragmentRelatorio extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
+
     private String mParam1;
     private String mParam2;
 
@@ -31,15 +25,7 @@ public class FragmentRelatorio extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment RelatoriosFragment.
-     */
-    // TODO: Rename and change types and number of parameters
+
     public static FragmentRelatorio newInstance(String param1, String param2) {
         FragmentRelatorio fragment = new FragmentRelatorio();
         Bundle args = new Bundle();
